31ff670076b4bceced35a22552e8329f4c093dc1
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-09-26 15:15:33 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+let name = confirm("Is your name Alex?")
+
+if (name) {
+    console.log("Marhamat");
+} else {
+    console.log("User don't aviabled");
+}
+
+let account = confirm("Is the account number is 7777?")
+
+if (account) {
+    console.log("Marhamat");
+} else {
+    console.log("Don't have this account");
+}
+
+let a = 10000
+
+let money = prompt("qancha pul obnalichit qilish")
+
+if (money) {
+    if (a > money) {
+        console.log("Hammasi chotki");
+    } else {
+        console.log("Pulling yetmaydi");
+    }
+} else {
+    console.log("Pulling yetmaydi");
+    let d = "Chan pul gifti"
+    let b = "Chan pul mond"
+}
+
+console.log(a);
+
+console.log(
+    a - money
+);
+
